4c3c6797da5c07f627d7217e2da73f6146ca5b1e
App Academy Student <appacademy@Apps-Mac-mini-103.local>
2016-01-26 18:45:10 -0500

    New component skeletons made for BoardDetailView and NewBoardIndexItem.

---
+++ frontend/components/newboardindexitem.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+NewBoardIndexItem = React.createClass({
+  render: function () {
+    return (
+      <li></li>
+    );
+  }
+});
+
+module.exports = NewBoardIndexItem;
