eaeb8653e8ab8acbb2ae76480a61f0778f4ed627
Michael Duke <michaelcduke@gmail.com>
2016-01-26 13:58:34 -0500

    CSS added to auth forms. Login requirement added for StaticPagesController#root.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
 
-  def root
+  before_action :require_logged_in
 
+  def root
   end
+
+  def require_logged_in
+    unless logged_in?
+      redirect_to new_sessions_url
+    end
+  end
+
 end
