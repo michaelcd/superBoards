7ceabf959aeb884f5a5f669d119cb840a572d5f8
Michael Duke <michaelcduke@gmail.com>
2016-01-29 11:45:48 -0500

    Basic List drag and drop functionality implemented. Still in progress but functional.

--- frontend/components/listwrapper.jsx
+++ frontend/components/listwrapper.jsx
@@ -3,25 +3,41 @@ var List = require('./list');
 var NewCard = require('./newcard');
 var DragSource = require('react-dnd').DragSource;
 var PropTypes = React.PropTypes;
+var ItemTypes = require('../constants/itemtypes');
+var DropTarget = require('react-dnd').DropTarget;
+var BoardDetailView = require('./boarddetailview');
+var ApiUtil = require('../util/api_util');
 
 // this.props.list
 // render list, aware of position
 
 var listTarget = {
   drop: function (props, monitor) {
-    moveList(props.ord);
+    var draggedList = monitor.getItem().list;
+    draggedList.ord = props.ord;
+    console.log(draggedList);
+    ApiUtil.moveList(draggedList);
   }
 };
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
 
 var ListWrapper = React.createClass({
   propTypes: {
     ord: PropTypes.number.isRequired,
   },
 
+
   render: function () {
     var x = this.props.ord;
+    var connectDropTarget = this.props.connectDropTarget;
 
-    return (
+    return connectDropTarget(
       <div className="list-wrapper">
         <List list={this.props.list} />
       </div>
@@ -29,4 +45,4 @@ var ListWrapper = React.createClass({
   }
 });
 
-module.exports = ListWrapper;
+module.exports = DropTarget(ItemTypes.LIST, listTarget, collect)(ListWrapper);
