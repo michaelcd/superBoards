2eff4724e9de43c8a3c9afa2bbf72a20734a03ca
Michael Duke <michaelcduke@gmail.com>
2016-02-04 18:47:04 -0500

    Logo in Navbar.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( logo_navbar.png )
+Rails.application.config.assets.precompile += %w( fullLogo.png )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -9,5 +11,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( logo.png )
