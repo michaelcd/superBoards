affaec4497a303fa1b3356528392093dbae8161e
Michael Duke <michaelcduke@gmail.com>
2016-02-25 14:57:03 -0500

    Adjust seed data. Fix create_demo_board call.

--- app/models/board.rb
+++ app/models/board.rb
@@ -10,8 +10,8 @@ class Board < ActiveRecord::Base
   has_many :cards, through: :lists
   # board has many users, through board shares
 
-  def self.create_demo_board
-    user_id = current_user.id
+  def self.create_demo_board(user)
+    user_id = user.id
     demo_board = Board.create(title: "Welcome to superBoards. Click this board!", author_id: user_id)
     demo_list_1 = demo_board.lists.create(title: "Cards", ord: 0)
     demo_list_2 = demo_board.lists.create(title: "Lists", ord: 1)
@@ -29,7 +29,7 @@ class Board < ActiveRecord::Base
     demo_list_3.cards.create(title: "Sharing options are available in the board menu.", ord: 2, author_id: user_id)
     demo_list_3.cards.create(title: "Once you're finished with a board, you can archive it.", ord: 3, author_id: user_id)
     demo_list_3.cards.create(title: "Please note, only the owner of a board can share or archive it.", ord: 4, author_id: user_id)
-    demo_card.comments.create(body: "Cards can also have comments.", author_id: guestuser1.id)
-    demo_card.comments.create(body: "All users that have access to a board can leave comments.", author_id: guestuser2.id)
+    demo_card.comments.create(body: "Cards can also have comments.", author_id: 2)
+    demo_card.comments.create(body: "All users that have access to a board can leave comments.", author_id: 2)
   end
 end
